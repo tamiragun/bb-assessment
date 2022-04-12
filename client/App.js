6ad40560b88bb42acdad399cd263482811d3804e
Tamira <t.gunzburg@gmail.com>
2022-04-12 14:53:01 +0200

    Add basic styling

--- client/App.js
+++ client/App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import Form from "./components/Form.js";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image
+        source={require("./assets/movie-icon.png")}
+        style={styles.image}
+      ></Image>
+      <Text style={styles.header}>My movies</Text>
       <Text>Add a movie you have watched:</Text>
       <StatusBar style="auto" />
       <Form></Form>
@@ -17,6 +22,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+    marginTop: 80,
+  },
+  image: {
+    width: 60,
+    height: 60,
+    padding: 10,
+  },
+  header: {
+    fontSize: 40,
+    fontWeight: "bold",
+    marginBottom: 60,
   },
 });
